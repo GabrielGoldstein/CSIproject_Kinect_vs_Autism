6ae2b6bd6ae2108325bfb02232968eb67f063690
Stuart <themastrgamr@gmail.com>
2015-11-16 15:58:36 -0500

    11/16 Update
    Did stuff

---
+++ Assets/KinectDemos/InteractionDemo/Scripts/PecCard.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PecCard : MonoBehaviour {
+
+	GrabDropScript grabScript;
+
+	public int snapCounter = 0;
+	public int numPieces = 0;
+	public int arraySize = 0;
+	public bool bodyMatchMode = true;
+
+	public GameObject[] match;
+	public GameObject[] pec ;
+
+	// Use this for initialization
+	void Start () {
+		//pec = new GameObject[arraySize];
+		grabScript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<GrabDropScript>();
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		if(snapCounter == numPieces){
+			Debug.Log("BEGIN PEC MODE");
+			bodyMatchMode = false;
+			//p2Holder.SetActive(true);
+			for (int i = 0; i < pec.Length; i++) {
+				//pec.GetLength(arraySize)
+				pec[i].SetActive(true);
+
+			}
+
+		}
+
+
+		if (match[0] != null && match[1] != null){ 
+			if (match[0].name == match[1].name) {
+				//START HEAD PART 
+				Debug.Log ("CONTINUE");
+			}
+			else {
+
+				Debug.Log ("ELSESLE");
+				Debug.Log (match[0].gameObject.GetComponent<Zzero>().origin);
+
+				//Sets PEC cards back to there original positions
+				match[0].gameObject.transform.position = match[0].gameObject.GetComponent<Zzero>().origin;
+				match[1].gameObject.transform.position = match[1].gameObject.GetComponent<Zzero>().origin;
+
+				//Restores PEC cards grab
+				grabScript.draggableObjects[0] = match[0];
+				grabScript.draggableObjects[1] = match[1];
+
+				match[0] = null;
+				match[1] = null;
+
+
+
+
+			}
+		}
+
+	}
+
+}
---
+++ Assets/KinectDemos/InteractionDemo/Scripts/PecMatch.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PecMatch : MonoBehaviour {
+
+	GrabDropScript grabScript;
+	PecCard pecScript;
+
+	Color color;
+	Renderer rend;
+	public Vector3 partOrigin;
+	public GameObject emptyObject;
+//	public AudioClip snap;
+	Vector3 position;
+
+	bool arraySet = true;
+
+	MatchingModel matchTransaction;
+
+	static bool isWaiting;
+
+	public static bool IsWaiting {
+		get { return isWaiting; }
+		set { isWaiting = value; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		matchTransaction = new MatchingModel();
+		grabScript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<GrabDropScript>();
+		pecScript = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<PecCard>();
+		rend = GetComponent<Renderer>();
+		partOrigin = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//determine a snap, send to MatchingModel.snap
+
+	}
+
+	void OnTriggerStay(Collider other) {
+		//if a piece is let go
+		if (grabScript.isGrabbed == false)
+		{
+			//PEC matches placeholder TAG
+			if(other.gameObject.tag == gameObject.tag) {
+				other.GetComponent<Zzero>().isSnapped = true;
+				rend.material.color = color;
+				position = gameObject.transform.position; //set to placeholder's position
+				other.gameObject.transform.position = position;
+				if(other.GetComponent<Zzero>().isSnapped){
+					//other.GetComponent<AudioSource>().PlayOneShot(snap);
+
+					if (arraySet)
+					{
+						if (pecScript.match[0] == null)
+							pecScript.match[0] = other.gameObject;
+						else if (pecScript.match[1] == null)
+							pecScript.match[1] = other.gameObject;
+
+						arraySet = false;
+					}
+
+					for(int i = 0; i < grabScript.draggableObjects.Length; i++){
+						if(other.gameObject.tag == grabScript.draggableObjects[i].tag)
+						{
+							Debug.Log ("Current GameObject " + grabScript.draggableObjects[i].tag);
+							grabScript.draggableObjects[i] = emptyObject;
+							//matchTransaction.setSnapped(other.gameObject, i);
+						}
+					}
+
+				}
+				Debug.Log("Position " + position);
+				//The line below delays a check for an incorrect piece
+				//prevents multiple correct/incorrect piece placement
+				
+			} 
+//			else if (other.GetComponent<Zzero>().isSnapped == false){
+//				//StartCoroutine(delayReset(other));
+//				other.gameObject.transform.position = other.GetComponent<Zzero>().origin;
+//				//other.GetComponent<AudioSource>().PlayOneShot (boing);
+//				Debug.Log("incorrect");
+//			}
+		}
+
+	}
+
+}
