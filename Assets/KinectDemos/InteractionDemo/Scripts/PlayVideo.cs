12a5e9a88b41decdbdb896c5b343b4bd7a0a4fae
GabrielGoldstein <gabriel.goldstein@cix.csi.cuny.edu>
2016-09-27 11:41:28 -0400

    9-25-16
    -Update PEC in LVL1
    -Movie Reward Fixed and Updated

--- Assets/KinectDemos/InteractionDemo/Scripts/PlayVideo.cs
+++ Assets/KinectDemos/InteractionDemo/Scripts/PlayVideo.cs
@@ -1,94 +1,77 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-
-
-[RequireComponent (typeof(AudioSource))]
+[RequireComponent (typeof(AudioSource))]    //If current object does not have a AudioSource, it creates one
 
 public class PlayVideo : MonoBehaviour {
 
-    private AudioSource audio;
-    public GameObject video;
-    public GameObject button;
-    public MovieTexture[] movies;
+    //Programmer: Gabriel Goldstein, Ivan Cuenca
 
+    //This Script is attended to be attached to Misc -> MainCamera
+    //VARIABLES NEED TO BE ASSIGNED IN EDITOR:  GameObject video
+    //                                          GameObject button
+
+    // SUMMARY: Handles the playing of a reward video when selected in the Results page after completing a level
+    // FUNCTION: 	- Assigns all videos in file to array movies
+    //              - Plays the video and audio of the movie
+    //              - Pressing Space stops the video
+
+    //NOTE FOR COMMENTS:    
+    //      Movie refers to combination of video and audio
+    //
+    //RECENT CHANGES:
+    //  Switched attached object from RawImage to MainCamera
+    //  Reduced un needed code
+    //  Took out single line functions
+
+    /* THINGS TO ADD: */
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    private AudioSource audio;      //Audio of the Movie
+    public GameObject video;        //The RawImage that displays the Movie (Assigned in the Editor)
+    public GameObject button;       //The Reward Button from the Results Panel (Assigned in the Editor)
+    private MovieTexture[] movies;  //Array of all Movies in the Folder (Resources -> Videos)
     
-
-    int n;
-
-    // Use this for initialization
+    int randNum; //Random Number used to randomly select a Movie
+    
     void Start () {
-		movies = Resources.LoadAll<MovieTexture>("Videos");
-
+		movies = Resources.LoadAll<MovieTexture>("Videos"); //Stores all Movies in the Folder (Resources -> Videos) in to the Array Movies
+        audio = video.GetComponent<AudioSource>(); //Gets videos AudioSource
     }
 	
-	// Update is called once per frame
 	void Update () {
-
         //Hitting Space will stop the video
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-			movies[n].Stop();
-
-            Disable_Video();
-            Enable_Button();
+        if (Input.GetKeyDown(KeyCode.Space)) {
+			movies[randNum].Stop(); //Stops the Movie
+            video.SetActive(false); //Deactivates the RawImage
+            button.SetActive(true); //Activates the Button
         }
         
         //If the video ends, then hide the video and show the play button
-        if (!movies[n].isPlaying)
-        {
-            Disable_Video();
-            Enable_Button();
+        if (!movies[randNum].isPlaying){
+            video.SetActive(false);
+            button.SetActive(true);
         }
     }
 
-	//Button Function to Play Videos
-	public void playVideo()
-	{
-		Button_Click();
-		Enable_Video();
-		Disable_Button();
-	
-	}
-
-    //To hide the video
-    public void Disable_Video()
-    {
-        video.SetActive(false);
-    }
-
-    //To reveal the video
-    public void Enable_Video()
-    {
+    public void playVideo() {
+        Button_Click();
         video.SetActive(true);
-    }
-
-    //To hide the UI button
-    public void Disable_Button()
-    {
         button.SetActive(false);
     }
 
-    //To reveal the UI button
-    public void Enable_Button()
-    {
-        button.SetActive(true);
-    }
+    
+    public void Button_Click() {
+        randNum = Random.Range(0, movies.Length); //Creates a Random Number to Randomly select a Movie from the Movies array
+                
+        video.GetComponent<RawImage>().texture = movies[randNum] as MovieTexture; //Sets the video for the RawImage
+        audio.clip = movies[randNum].audioClip; //Sets the audioclip for the Movie to the AudioSource
 
-    //When clicked on the UI button do this:
-    public void Button_Click()
-    {
-        //Assign n to a random number from 0-2
-        n = Random.Range(0, movies.Length);
-
-        //Display that number on the console (so we know what video gets played)
-        print(n);
-        
-        GetComponent<RawImage>().texture = movies[n] as MovieTexture;
-        audio = GetComponent<AudioSource>();
-        audio.clip = movies[n].audioClip;
-
-        movies[n].Play();
+        //Plays Video and Audio
+        movies[randNum].Play();
         audio.Play();
     }
 
